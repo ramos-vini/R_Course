547a57400cad0d1dc5b76149b757c4952309b1ab
ramos-vini <ramoscontrole@gmail.com>
2023-06-07 15:55:03 +0200

    Variables

--- main.R
+++ main.R
@@ -1,1 +1,8 @@
-# First Commit
+# Variables
+# Assignment Operator in R: = or <-
+
+a = 1 + 2
+A <- 5
+
+# rm(argument) -> Removes a variable from the environment
+# rm(list = ls()) -> Clears the Global Environment
